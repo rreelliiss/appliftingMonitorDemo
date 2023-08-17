f3705a7cb4f20003ea8d9676d40ffc116cbf9bc8
Jakub Šiller <siller.jakub@gmail.com>
2023-08-17 08:56:20 +0200

    spring app created

---
+++ src/main/java/com/siller/applifting/monitor/MonitorApplication.java
@@ -0,0 +1,13 @@
+package com.siller.applifting.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorApplication.class, args);
+	}
+
+}
